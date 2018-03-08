baf16a47e6b6a825a9f8021853cfd7ae9ee588f4
Jim <jim@advancebass.com>
2018-03-07 23:12:29 -0800

    testing images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,17 @@ module.exports = {
     title: 'Gatsby WP Starter',
     subtitle: 'ham',
   },
-  plugins: ['gatsby-plugin-react-helmet',
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   {
     resolve: "gatsby-source-wordpress",
     options: {
       baseUrl: "catkittycat.com",
       protocol: 'http',
       hostingWPCOM: false,
-      useACF: false,
+      useACF: true,
       verboseOutput: true
     }
   }],
